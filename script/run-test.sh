0fb4fa140a4760e6637b04a523f1797ada977ad7
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-02-28 19:50:06 +0900

    nginx

--- script/run-test.sh
+++ script/run-test.sh
@@ -4,34 +4,62 @@ set -e
 STUDENT_IMAGE=${STUDENT_IMAGE:=student-http}
 DOCKER=${DOCKER:=docker}
 
-
 PROJECT_PATH=$(dirname "$0")/..
 PROJECT_PATH=$(realpath "${PROJECT_PATH}")
+
+SRC_PATH=${SRC_PATH:=${PROJECT_PATH}/src}
 SRV_PATH=${SRV_PATH:=${PROJECT_PATH}/srv}
+ETC_PATH=${ETC_PATH:=${PROJECT_PATH}/etc}
 mkdir -p ${SRV_PATH}
+
+MYUID=$(id -u)
+MYGID=$(id -g)
 
 cd "${PROJECT_PATH}"
 
-build(){
-    cd "${PROJECT_PATH}/src"
+build_local() {
+    cd "${SRC_PATH}"
     make clean
     make
+    cd "${PROJECT_PATH}"
 }
 
-build_docker(){
-    cd "${PROJECT_PATH}/src"
+build_container() {
+    cd "${SRC_PATH}"
     ${DOCKER} build -t ${STUDENT_IMAGE} .
+    cd "${PROJECT_PATH}"
 }
 
+down_local() {
+    pkill -x "${SRC_PATH}/http-server" 2>/dev/null || true
+}
 
-up_local(){
+down_container() {
+    ${DOCKER} container rm -f ${STUDENT_IMAGE}-server 2>/dev/null || true
+    ${DOCKER} container rm -f ${STUDENT_IMAGE}-nginx 2>/dev/null || true
+    ${DOCKER} network rm ${STUDENT_IMAGE}-network 2>/dev/null || true
+
+}
+
+down() {
+    down_local
+    down_container
+}
+
+up_local() {
     cd "${SRV_PATH}"
-    "${PROJECT_PATH}/http-server" &
+    "${SRC_PATH}/http-server" &
 }
 
-up_container(){
-    ${DOCKER} network rm ${STUDENT_IMAGE}-network 2> /dev/null || true
-    ${DOCKER} container rm -f ${STUDENT_IMAGE}-server 2> /dev/null || true
-    ${DOCKER} run --rm -d -v${SRV_PATH}:/srv --name ${STUDENT_IMAGE}-server ${STUDENT_IMAGE}
-
+up_network() {
+    ${DOCKER} network create ${STUDENT_IMAGE}-network
 }
+
+up_container() {
+    ${DOCKER} container run --rm -t --network ${STUDENT_IMAGE}-network -v${SRV_PATH}:/srv --name ${STUDENT_IMAGE}-server ${STUDENT_IMAGE} &
+}
+
+up_nginx() {
+    ${DOCKER} container run --rm -t --network ${STUDENT_IMAGE}-network -v${SRV_PATH}:/srv --name ${STUDENT_IMAGE}-nginx -v${ETC_PATH}:/config/nginx/site-confs -e PUID=${MYUID} -e PGID=${MYGID} linuxserver/nginx:latest &
+}
+
