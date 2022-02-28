151c860c9795121e72b369faed9f05cbc5ab7d61
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-02-28 21:43:23 +0900

    scripts...

--- script/run-test.sh
+++ script/run-test.sh
@@ -30,13 +30,23 @@ build_container() {
     cd "${PROJECT_PATH}"
 }
 
+copy_to_volume () {
+    docker run --rm -v$1:/from:ro -v$2:/to busybox:stable sh -c "rm -rf /to/*;cp -r /from/* /to/;chown -R 101:101 /to/"
+}
+
+copy_from_volume () {
+    docker run --rm -v$2:/from:ro -v$1:/to busybox:stable sh -c "rm -rf /to/*;cp -r /from/* /to/;chown -R ${MYUID}:${MYGID} /to/"
+}
+
 down_local() {
     pkill -x "${SRC_PATH}/http-server" 2>/dev/null || true
 }
 
 down_container() {
     ${DOCKER} container rm -f ${STUDENT_IMAGE}-server 2>/dev/null || true
+    ${DOCKER} volume rm ${STUDENT_IMAGE}-server 2>/dev/null || true
     ${DOCKER} container rm -f ${STUDENT_IMAGE}-nginx 2>/dev/null || true
+    ${DOCKER} volume rm ${STUDENT_IMAGE}-nginx 2>/dev/null || true
     ${DOCKER} network rm ${STUDENT_IMAGE}-network 2>/dev/null || true
 
 }
@@ -56,10 +66,24 @@ up_network() {
 }
 
 up_container() {
-    ${DOCKER} container run --rm -t --network ${STUDENT_IMAGE}-network -v${SRV_PATH}:/srv --name ${STUDENT_IMAGE}-server ${STUDENT_IMAGE} &
+    ${DOCKER} volume create ${STUDENT_IMAGE}-server
+    ${DOCKER} container run --rm -t --network ${STUDENT_IMAGE}-network -v${STUDENT_IMAGE}-server:/srv --name ${STUDENT_IMAGE}-server ${STUDENT_IMAGE} &
 }
 
 up_nginx() {
-    ${DOCKER} container run --rm -t --network ${STUDENT_IMAGE}-network -v${SRV_PATH}:/srv --name ${STUDENT_IMAGE}-nginx -v${ETC_PATH}:/config/nginx/site-confs -e PUID=${MYUID} -e PGID=${MYGID} linuxserver/nginx:latest &
+    ${DOCKER} volume create ${STUDENT_IMAGE}-nginx
+    copy_to_volume ${SRV_PATH} ${STUDENT_IMAGE}-nginx
+    ${DOCKER} container run --rm -d --network ${STUDENT_IMAGE}-network -v${STUDENT_IMAGE}-nginx:/srv --name ${STUDENT_IMAGE}-nginx -v${ETC_PATH}:/etc/nginx/conf.d/:ro nginx:latest
 }
 
+run_curl() {
+    docker run --rm --network ${STUDENT_IMAGE}-network curlimages/curl:latest ${VERBOSE_ARG} -s -v  -H 'Expect:' $@
+}
+
+
+# down
+# up_network
+# up_nginx
+# sleep 5
+# run_curl  http://student-http-nginx:8080/test6.html
+# copy_from_volume ${SRV_PATH}2 ${STUDENT_IMAGE}-nginx
