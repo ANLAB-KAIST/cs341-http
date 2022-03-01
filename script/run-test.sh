99db766a036a7b8261904232baa61f2eaf645a71
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-03-01 21:41:29 +0900

    test...

--- script/run-test.sh
+++ script/run-test.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 set -e
 
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <test_name> <server> <client>"
+    exit 1
+fi
+
 STUDENT_IMAGE=${STUDENT_IMAGE:=student-http}
+SERVER_NAME=${STUDENT_IMAGE}-server
+CLIENT_NAME=${STUDENT_IMAGE}-client
+NETWORK_NAME=${STUDENT_IMAGE}-network
+
 DOCKER=${DOCKER:=docker}
 
 PROJECT_PATH=$(dirname "$0")/..
 PROJECT_PATH=$(realpath "${PROJECT_PATH}")
 
 SRC_PATH=${SRC_PATH:=${PROJECT_PATH}/src}
-SRV_PATH=${SRV_PATH:=${PROJECT_PATH}/srv}
 ETC_PATH=${ETC_PATH:=${PROJECT_PATH}/etc}
-mkdir -p ${SRV_PATH}
-
-CREATE_VOLUME=${CREATE_VOLUME:=true}
+SRV_PATH=/tmp/${STUDENT_IMAGE}-srv
 
 MYUID=$(id -u)
 MYGID=$(id -g)
 
+DO_PULL=${DO_PULL:=true}
+
 cd "${PROJECT_PATH}"
-
-
-docker pull busybox:stable
-docker pull nginx:stable
-docker pull curlimages/curl:latest
-
 
 build_local() {
     cd "${SRC_PATH}"
@@ -38,12 +40,10 @@ build_container() {
     cd "${PROJECT_PATH}"
 }
 
-copy_to_volume() {
-    docker run --rm -v$1:/from:ro -v$2:/to busybox:stable sh -c "rm -rf /to/*;cp -r /from/* /to/;chown -R 101:101 /to/"
-}
-
-copy_from_volume() {
-    docker run --rm -v$2:/from:ro -v$1:/to busybox:stable sh -c "rm -rf /to/*;cp -r /from/* /to/;chown -R ${MYUID}:${MYGID} /to/"
+prepare_volume() {
+    ${DOCKER} volume rm $2 2>/dev/null || true
+    ${DOCKER} volume create $2
+    ${DOCKER} run --rm -v$1:/from:ro -v$2:/to busybox:stable sh -c "rm -rf /to/*;cp -r /from/* /to/;chown -R 101:101 /to/"
 }
 
 down_local() {
@@ -51,12 +51,11 @@ down_local() {
 }
 
 down_container() {
-    ${DOCKER} container rm -f ${STUDENT_IMAGE}-server 2>/dev/null || true
-    ${CREATE_VOLUME} && ${DOCKER} volume rm ${STUDENT_IMAGE}-server 2>/dev/null || true
-    ${DOCKER} container rm -f ${STUDENT_IMAGE}-nginx 2>/dev/null || true
-    ${CREATE_VOLUME} && ${DOCKER} volume rm ${STUDENT_IMAGE}-nginx 2>/dev/null || true
-    ${DOCKER} network rm ${STUDENT_IMAGE}-network 2>/dev/null || true
-
+    ${DOCKER} container rm -f ${SERVER_NAME} 2>/dev/null || true
+    ${DOCKER} volume rm ${SERVER_NAME} 2>/dev/null || true
+    ${DOCKER} container rm -f ${CLIENT_NAME} 2>/dev/null || true
+    ${DOCKER} volume rm ${CLIENT_NAME} 2>/dev/null || true
+    ${DOCKER} network rm ${NETWORK_NAME} 2>/dev/null || true
 }
 
 down() {
@@ -65,46 +64,92 @@ down() {
 }
 
 up_local() {
+    mkdir -p "${SRV_PATH}"
+    cp ${1}/* ${SRV_PATH}
     cd "${SRV_PATH}"
     "${SRC_PATH}/http-server" &
 }
 
 up_network() {
-    ${DOCKER} network create ${STUDENT_IMAGE}-network
+    ${DOCKER} network create ${NETWORK_NAME}
 }
 
-up_container_inner() {
-    ${CREATE_VOLUME} && ${DOCKER} volume create ${STUDENT_IMAGE}-server
-    ${CREATE_VOLUME} && copy_to_volume ${SRV_PATH} ${STUDENT_IMAGE}-server
-    ${DOCKER} container run --rm -t --network ${STUDENT_IMAGE}-network -v${STUDENT_IMAGE}-server:/srv${1} --name ${STUDENT_IMAGE}-server ${STUDENT_IMAGE} &
+up_container() {
+    ${DOCKER} container run --rm -t --network ${NETWORK_NAME} -v${SERVER_NAME}:/srv --name ${SERVER_NAME} ${STUDENT_IMAGE} &
 }
 
-up_nginx_inner() {
-    ${CREATE_VOLUME} && ${DOCKER} volume create ${STUDENT_IMAGE}-nginx
-    ${CREATE_VOLUME} && copy_to_volume ${SRV_PATH} ${STUDENT_IMAGE}-nginx
-    ${DOCKER} container run --rm -d --network ${STUDENT_IMAGE}-network -v${STUDENT_IMAGE}-nginx:/srv${1} --name ${STUDENT_IMAGE}-nginx -v${ETC_PATH}:/etc/nginx/conf.d/:ro nginx:stable
-}
-
-up_container_rw(){
-    up_container_inner
-}
-up_container_ro(){
-    up_container_inner ":ro"
-}
-
-up_nginx_rw(){
-    up_nginx_inner
-}
-up_nginx_ro(){
-    up_nginx_inner ":ro"
+up_nginx() {
+    ${DOCKER} container run --rm -d --network ${NETWORK_NAME} -v${SERVER_NAME}:/srv --name ${SERVER_NAME} -v${ETC_PATH}:/etc/nginx/conf.d/:ro nginx:stable
 }
 
 run_curl() {
-    docker run --rm --network ${STUDENT_IMAGE}-network curlimages/curl:latest ${VERBOSE_ARG} -s -v -H 'Expect:' $@
+     ${DOCKER} run --rm --network ${NETWORK_NAME} -v${CLIENT_NAME}:/tmp --name ${CLIENT_NAME} curlimages/curl:latest -s -v -H 'Expect:' $@
+}
+
+run_client() {
+    ${DOCKER} run --rm --network ${NETWORK_NAME} -v${CLIENT_NAME}:/tmp --name ${CLIENT_NAME} ${STUDENT_IMAGE} $@
 }
 
 run_nc() {
-     docker run --rm --network ${STUDENT_IMAGE}-network busybox:stable nc $@
+    ${DOCKER} run --rm --network ${NETWORK_NAME} -v${CLIENT_NAME}:/tmp --name ${CLIENT_NAME} busybox:stable nc $@
 }
 
+test_get_hello() {
+    prepare_volume
 
+}
+
+case $1 in
+get_hello)
+    SERVER_DATA="${PROJECT_PATH}/tests/01"
+    COMMAND="-o /tmp/index.html http://${SERVER_NAME}:8080/index.html"
+    CHECK="index.html"
+    ;;
+*)
+    echo "Unkown test: $1"
+    exit 1
+    ;;
+esac
+
+case $2 in
+
+nginx)
+    UP_SERVER=up_nginx
+    ;;
+
+student)
+    UP_SERVER=up_container
+    ;;
+*)
+    echo "Unkown server: $2"
+    exit 1
+    ;;
+esac
+
+case $3 in
+
+curl)
+    RUN_CLI=run_curl
+    ;;
+
+student)
+    RUN_CLI=run_client
+    ;;
+*)
+    echo "Unkown client: $3"
+    exit 1
+    ;;
+esac
+
+${DO_PULL} && ${DOCKER} pull busybox:stable
+${DO_PULL} && ${DOCKER} pull nginx:stable
+${DO_PULL} && ${DOCKER} pull curlimages/curl:latest
+
+build_container
+down
+[ ! -z "$SERVER_DATA" ] && prepare_volume "$SERVER_DATA" "$SERVER_NAME"
+[ ! -z "$CLIENT_DATA" ] && prepare_volume "$CLIENT_DATA" "$CLIENT_NAME"
+up_network
+${UP_SERVER}
+sleep 5
+${RUN_CLI} ${COMMAND}
